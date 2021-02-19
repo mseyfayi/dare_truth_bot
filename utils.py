e3fde0b1ef77f7109ac7280dafa849fbad8fab0d
Mohammad Seyfayi <madsams024@gmail.com>
2021-02-19 13:32:42 +0330

    Union removed

--- utils.py
+++ utils.py
@@ -1,14 +1,14 @@
-from typing import Optional, List, Union
+from typing import Optional, List
 
 from telegram import InlineKeyboardButton
 
 from bot.callback_data import callbacks
 
 
-def build_menu(buttons: List[Union[InlineKeyboardButton]], n_cols: int,
-               header_buttons: Optional[List[Union[InlineKeyboardButton]]] = None,
-               footer_buttons: Optional[List[Union[InlineKeyboardButton]]] = None) \
-        -> List[List[Union[InlineKeyboardButton]]]:
+def build_menu(buttons: List[InlineKeyboardButton], n_cols: int,
+               header_buttons: Optional[List[InlineKeyboardButton]] = None,
+               footer_buttons: Optional[List[InlineKeyboardButton]] = None) \
+        -> List[List[InlineKeyboardButton]]:
     menu = [buttons[i:i + n_cols] for i in range(0, len(buttons), n_cols)]
     if header_buttons:
         menu.insert(0, header_buttons)
@@ -23,7 +23,7 @@ def create_inline_button(
         callback_data_name: Optional[str] = None,
         switch_inline_query: Optional[str] = None,
         callback_data_creator_payload: Optional[any] = None
-) -> Union[InlineKeyboardButton]:
+) -> InlineKeyboardButton:
     btn = buttons[name]
     if switch_inline_query is not None:
         return InlineKeyboardButton(btn, switch_inline_query=switch_inline_query)
